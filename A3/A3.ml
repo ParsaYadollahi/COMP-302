a6d6230366b737b87b123942c034d958c71e21a3
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2021-03-02 14:19:02 -0500

    q2

--- A3/A3.ml
+++ A3/A3.ml
@@ -9,7 +9,7 @@ let rec find_map (f : 'a -> 'b option) (l : 'a list) : 'b option =
       | None -> find_map f xs
 ;;
 
-let par x = if x/2 = 0 then true else false ;;
+let par x = (x / 2) = 0 ;;
 let lst = [1;2;3;4;5;6;7;8;9]
 
 let partition (p : 'a -> bool) (l : 'a list) : ('a list * 'a list) =
@@ -28,25 +28,26 @@ let make_manager (masterpass : masterpass) : pass_manager =
   let ref_list : (address * password) list ref = ref [] in
   let ref_master = ref masterpass in
   let counter = ref 0 in
-  let failed = ref 0 in
+  let failed_counter = ref 0 in
   let verify_pass masterpass_input =
     if masterpass_input = !ref_master then counter := !counter + 1
     else (
-      if !failed > 2 then raise AccountLocked
-      else raise WrongPassword; failed := !failed + 1
+      if !failed_counter > 2 then raise AccountLocked
+      else raise WrongPassword; failed_counter := !failed_counter + 1
     )
   in
   let save master address password_in =
-    if !failed < 3 then (verify_pass master; ref_list := (address, encrypt master password_in) :: !ref_list)
+    if !failed_counter < 3 then (verify_pass master; ref_list := (address, encrypt master password_in) :: !ref_list)
     else raise AccountLocked
   in
   let get_force master address =
-    let f = fun tuple -> let (add, pass) = tuple in if address = add then
-    Some (decrypt master pass) else
-    None in
+    let f = fun tuple -> let (add, pass) = tuple in
+     if address = add then
+      Some (decrypt master pass) else
+      None in
     find_map f !ref_list in
   let get master address =
-    if !failed < 3 then (
+    if !failed_counter < 3 then (
       verify_pass master;
       get_force master address
     )
@@ -54,13 +55,13 @@ let make_manager (masterpass : masterpass) : pass_manager =
   in
   let update_master m1 m2 = verify_pass m1;
   List.map (
-    fun x -> match x with
-      | (_,z) -> encrypt m2 (decrypt m1 z)
+    fun x -> let (_,z) = x  in
+      encrypt m2 (decrypt m1 z)
     ) !ref_list;
     ref_master := m2
   in
   let count_ops master =
-    if !failed < 3 then (verify_pass master; !counter)
+    if !failed_counter < 3 then (verify_pass master; !counter)
     else raise AccountLocked
   in {save; get_force; get; update_master; count_ops}
 ;;
