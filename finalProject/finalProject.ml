1b645681f55daf593f432a6ff58530d1f86cecce
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2021-04-30 22:59:25 -0400

    Last time.... OCAML A BISH

--- finalProject/finalProject.ml
+++ finalProject/finalProject.ml
@@ -1,21 +1,16 @@
+(*
+  Parsa Yadollahi -260869949
+  COMP 302 - Final Project
+ *)
 (* Q0  : Get familiar with the external syntax of MiniML *)
 let parse_tests : (string * (string, exp) either) list = [
     (* Provide your tests for the parser *)
-  ("1;", Right (Int 1));
-  ("1 = 1;", Right (Primop (Equals, [Int 1; Int 1])));
-  ("1 < 2;", Right (Primop (LessThan, [Int 1; Int 2])))
+  ("1;", Right (Int 1))
 ]
 
 
 let free_vars_tests : (exp * name list) list = [
-  (Int 10, []);
-  ((Anno (If (Bool false, Tuple [], Tuple []), TInt)), [] );
-  ((Apply (Apply (Var "Parsa", Fn ("Yadollahi", None, Primop (Times, []))), Int 12)), ["Parsa"]);
-  ((Fn ("x", None, Var "x")),[]);
-  ((Let ([Valtuple (Int 4, ["x"; "y"; "z"]); Val (Var "z", "n2")], Primop (Plus, [Primop (Plus, []); Var "y"]))), []);
-  ((Let ([Val (Int 8, "x")], Let ([Val (Primop (Plus, []), "x")], Primop (Plus, [])))) ,[]);
-  ((Fn ("x", None, Primop (Plus, [Var "x"; Var "y"]))), ["y"]);
-  ((Fn ("y", None, Apply (Var "x", Var "y"))),["x"]);
+  (Int 10, [])
 ]
 
 (* Q1  : Find the free variables in an expression *)
@@ -44,12 +39,15 @@ let rec free_vars (e : exp) : name list = match e with
       let deleteVars = free_vars el3 in
       delete [el1] deleteVars
   | Let (l, el) ->
-      let rec aux l1 l2 l3 = match l1 with
+      let rec aux l1 l2 l3 =
+      match l1 with
         | [] -> (l2, l3)
-        | x::remainder -> match x with
+        | x :: remainder -> match x with
           | Valtuple (el1, el2) ->
               let union1 = union l2 el2 in
-              let union2 = union l3 (delete l2 (free_vars el1)) in
+              let freeVarEl1 = free_vars el1 in
+              let del = delete l2 freeVarEl1 in
+              let union2 = union l3 del in
               aux remainder (union1) (union2)
           | Val(el1, el2) | ByName (el1, el2) ->
               let deleteVars = delete l2 (free_vars el1) in
@@ -61,13 +59,7 @@ let rec free_vars (e : exp) : name list = match e with
       union f (delFreeVars)
 
 
-let unused_vars_tests : (exp * name list) list = [
-
-  ((Let ([Valtuple (Tuple [Primop (Plus, [Int 2; Int 1]); Primop (Times, [Int 2; Var "z"])], ["x"; "z"])], Primop (Times, [Var "x"; Var "y"]))) ,  ["z"] );
-  ((Let ([Val (Rec ("ryan", TArrow (TInt, TInt), Fn ("n", Some TInt, If (Primop (Equals, [Var "n"; Int 0]), Var "x", Apply (Var "ryan", Primop (Minus, [Var "n"; Int 1]))))), "ryan")], Int 3)), ["ryan"]);
-  ((Anno (If (Bool true, Tuple [Var "x1"; Var "x2"; Var "x3"], Tuple [Var "x4"; Var "x5"]), TInt)) ,  [] );
-  ((Fn ("x", None, Int 3)), ["x"]);
-]
+let unused_vars_tests : (exp * name list) list = []
 
 (* Q2  : Check variables are in use *)
 let rec unused_vars (e : exp) : name list = match e with
@@ -119,8 +111,8 @@ let rec unused_vars (e : exp) : name list = match e with
               let appendL3 = l3 @ (unusedVars) in
               let delftn = (delSetFunction (freeVars1) (l2)) in
               let appendDelFtn = (delftn) @ [var2] in
-              aux remainder (appendDelFtn) (appendL3)
-      in let (u1, u2) = aux l [] [] in
+              aux remainder (appendDelFtn) (appendL3) in
+              let (u1, u2) = aux l [] [] in
       let delSetFuncVar = delSetFunction (free_vars vars) u1 in
       let unusedVars = unused_vars vars in
       delSetFuncVar @ unusedVars @ u2
@@ -134,7 +126,11 @@ let subst_tests : (((exp * name) * exp) * exp) list = [
 (* Q3  : Substitute a variable *)
 let rec subst ((e', x) : exp * name) (e : exp) : exp =
   match e with
-  | Var y -> Var y
+  | Var y ->
+      if x = y then
+        e'
+      else
+        Var y
   | Int _ | Bool _ -> e
   | Primop (po, es) -> Primop (po, List.map (subst (e', x)) es)
   | If (e1, e2, e3) -> If (subst (e', x) e1, subst (e', x) e2, subst (e', x) e3)
@@ -146,24 +142,15 @@ let rec subst ((e', x) : exp * name) (e : exp) : exp =
       let subst2 = subst (e', x) e2 in
       Apply (subst1, subst2)
   | Fn (y, t, e) ->
-      if y != x then
         let subEl = subst (e', x) in
-        let freeVar = free_vars e' in
         let freshVar = fresh_var y in
         let subVar = subEl (subst (Var (freshVar), y) e) in
         Fn (freshVar, t, subVar)
-      else
-        Fn (y, t, e)
   | Rec (y, t, e) ->
       let subEl = subst (e', x) in
-      let freeVars = free_vars e' in
-      let freeMem = member y freeVars in
-      if freeMem then
-        let freshVar = fresh_var y in
-        let subVar = subEl (subst (Var (freshVar), y) e) in
-        Rec (freshVar, t, subVar)
-      else
-        Rec (y, t, e)
+      let freshVar = fresh_var y in
+      let subVar = subEl (subst (Var (freshVar), y) e) in
+      Rec (freshVar, t, subVar)
 
 
 
@@ -178,33 +165,11 @@ let rec subst ((e', x) : exp * name) (e : exp) : exp =
               let newEl2 = substEl el2 in
               aux t (newEl2)
         in aux (List.rev list) expr in
-      let rec aux2 list1 list2 repl track = (
+      let rec aux2 list1 list2 repl tr = (
         match list1 with
-        | [] ->  (list2, repl, track)
-        | head :: remainder -> match head with
-          | Valtuple (y,n) ->
-              let rec find l1x l2x l3x trk = (match l1x with
-                  | [] -> (l2x, l3x, trk)
-                  | head :: remainder ->
-                      let z = fresh_var head in
-                      let concatZ = l2x @ [z] in
-                      let varZ = (Var(z), head) in
-                      let concatVarZ = l3x @ [varZ] in
-                      find remainder concatZ concatVarZ trk
-                ) in
-              let (ax, bx, tr1) =
-                find n [] [] false in
-              let replBX = repl @ bx in
-              let trackOrTr1 = track || tr1 in
-              let param = (
-                let replY = replacing repl y in
-                let substY = subst (e', x) (replY) in
-                let valTupleSubstY = Valtuple (( substY ), ax) in
-                let concatvalTupleSubstY = list2 @ [valTupleSubstY] in
-                concatvalTupleSubstY
-              ) in
-              aux2 remainder param replBX trackOrTr1
-          | ByName (y, n) ->
+        | [] ->  (list2, repl, tr)
+        | head :: remainder ->
+          let ByName (y, n) = head in
               let replY = replacing repl y in
               let substEl = subst (e', x) (replY) in
                 let byNameSubst = ByName ( ( substEl ), n) in
@@ -217,15 +182,15 @@ let rec subst ((e', x) : exp * name) (e : exp) : exp =
       Let (g, substReplHE2)
 
 let eval_tests : (exp * exp) list = [
-  ((Let ([Valtuple (Tuple [Int 1; Bool true], ["x"; "x"])], Var "x")), Bool true);
 ]
 
-let replacing l1 var =
+let replfunct l1 var =
   let rec aux l2 var2 = match l2 with
-    | [] -> var2
     | x :: remainder ->
-        let (b, c) = x in
-        aux remainder (subst (b, c) var2)
+        let (l1, l2) = x in
+        let substl = subst (l1, l2) var2 in
+        aux remainder substl
+    | [] -> var2
   in
   aux (List.rev l1) var
 
@@ -296,8 +261,8 @@ let rec eval : exp -> exp =
                 | [] -> el2
                 | x :: remainder -> match x with
                   | Val (var1, var2)  ->
-                      let replace = replacing el2 var1 in
-                      let evalReplace = [eval (replacing el2 var1), var2] in
+                      let replace = replfunct el2 var1 in
+                      let evalReplace = [eval (replfunct el2 var1), var2] in
                       let concatReplace = el2 @ evalReplace in
                       aux remainder (concatReplace)
                   | Valtuple (var1, var2) ->
@@ -308,7 +273,7 @@ let rec eval : exp -> exp =
                           let lenList2 = List.length var2 in
                           let rec substTuples l1 l2 repl = match (l1, l2) with
                             | (x_l1::remainderL1, y_l2::remainderL2) ->
-                                let replace = replacing repl y_l2 in
+                                let replace = replfunct repl y_l2 in
                                 let addReplace = [replace, x_l1] in
                                 let concatReplace = repl @ addReplace in
                                 substTuples remainderL1 remainderL2 concatReplace
@@ -317,7 +282,7 @@ let rec eval : exp -> exp =
                           aux remainder rep
                       | _ ->  stuck "Needed a tuple"
               in let repl = aux ds [] in
-              let el = replacing repl e
+              let el = replfunct repl e
               in eval el
     in
   (* do not change the code from here *)
@@ -377,22 +342,17 @@ let rec unify (ty1 : typ) (ty2 : typ) : unit =
   else
     match (ty1, ty2) with
     | TVar var, TVar var' -> (
-        match (!var, !var') with
+      let not_var = !var in
+      let not_var' = !var' in
+        match (not_var, not_var') with
         | Some (el), Some (el') -> unify el el'
         | Some (el'), None ->
             let comp = aux2 false el' var' in
-            if comp then
-              var':= !var
-            else
-              ()
-        | None, Some (gy')  ->  if aux2 false gy' var then  var := !var' else ()
+              var':= not_var
         | None, None ->
             let comp = var != var' in
-            if comp then
               let tVar = TVar var' in
               var := Some tVar
-            else
-              ()
       )
     | TVar var, type1 -> (
         match !var with
@@ -420,7 +380,7 @@ let rec unify (ty1 : typ) (ty2 : typ) : unit =
     | TProduct (list1'), TProduct (list2') ->
         let lenList1 = List.length list1' in
         let lenList2 = List.length list2' in
-        if  lenList1 = lenList2 then
+        if  lenList1 != lenList2 then
           type_fail "Length of Tuples are unequal"
         else
           let rec aux3 list1 list2 = match (list1, list2) with
@@ -447,8 +407,9 @@ let rec infer (ctx : context) (e : exp) : typ =  match e with
       unify unify1 TBool;
       unify unify2 unify3;
       unify2
-  | Primop (o, l1) -> (match o with
-      | Negate -> (match l1 with
+  | Primop (var1, list1) -> (
+    match var1 with
+      | Negate -> (match list1 with
           | [x] -> (
               let inferEl = infer ctx x in
               unify inferEl TInt;
@@ -456,20 +417,6 @@ let rec infer (ctx : context) (e : exp) : typ =  match e with
             )
           | _ ->
               type_fail "Error - Cannot negate: missinig arguments")
-      | Or | And -> (
-          if (
-            List.for_all (
-              fun el -> (
-                  let inferEl = infer ctx el in
-                  unify inferEl TBool;
-                  true
-                )
-            ) l1
-          ) then
-            TBool
-          else
-            type_fail "Error - Not all arguments are booleans"
-        )
       | Equals | NotEquals | LessThan | GreaterEqual | LessEqual | GreaterThan -> (
           if (
             List.for_all (
@@ -478,7 +425,7 @@ let rec infer (ctx : context) (e : exp) : typ =  match e with
                   unify inferEl TInt;
                   true
                 )
-            ) l1
+            ) list1
           )
           then
             TBool
@@ -493,7 +440,7 @@ let rec infer (ctx : context) (e : exp) : typ =  match e with
                   unify inferEl TInt;
                   true
                 )
-            ) l1
+            ) list1
           )
           then
             TInt
@@ -521,7 +468,8 @@ let rec infer (ctx : context) (e : exp) : typ =  match e with
           let inferExtend = extend ctx (extendType) in
           let inferEl = infer inferExtend el3 in
           TArrow (
-            type1, inferEl
+            type1,
+            inferEl
           )
       | None ->
           let extendctx = el1, fresh_tvar() in
@@ -533,73 +481,7 @@ let rec infer (ctx : context) (e : exp) : typ =  match e with
             inferEl3
           )
     )
-  | Rec (el1, el2, el3) -> el2
-  | Let (li,e1) ->
-      let rec aux map key =
-        let Ctx(m) = map in
-        match m with
-        | [] -> false
-        | (t, _)::remainder ->
-            if t = key then
-              true
-            else
-              aux (Ctx (remainder)) key
-      in
-      let rec fold_ex list1 list2 repl =
-        match list1 with
-        | [] -> (list2, repl)
-        | x :: remainder -> (
-            match x with
-            | Val (el1, el2) | ByName (el1, el2) -> (
-                if aux list2 el2 then (
-                  let naz = fresh_var el2 in
-                  let replaceEl1 = replacing repl el1 in
-                  let inferEl1 = infer list2 replaceEl1 in
-                  let extendInfer =  naz, inferEl1 in
-                  let extendList2 = extend list2 (extendInfer) in
-                  let elArray = [(Var (naz)), el2] in
-                  let concatElRepl = repl @ elArray in
-                  fold_ex remainder extendList2 concatElRepl
-                )
-                else (
-                  let replace = replacing repl el1 in
-                  let inferList2 = infer list2 replace in
-                  let extendInfer = el2, inferList2 in
-                  let extendList2 = extend list2 (extendInfer) in
-                  fold_ex remainder extendList2 repl
-                )
-              )
-            | Valtuple (l1, l2) -> (
-                let replace = replacing repl l1 in
-                let matching = infer list2 replace in
-                match matching with
-                | TProduct (l3) ->
-                    let lenList2 = List.length l2 in
-                    let lenList3 = List.length l3 in
-                    if lenList2 = lenList3 then
-                      type_fail "Error - Size of tuples must be equal"
-                    else let rec aux2 l1x l2x l3x rep =
-                           match (l1x, l2x) with
-                           | ([], _) -> (l3x, rep)
-                           | (head::tail), (head2::tail2) ->
-                               if aux l3x head then (
-                                 let gz = fresh_var head in
-                                 let extendL3x = extend l3x (gz,head2) in
-                                 let gzArray = [((Var (gz)), head)] in
-                                 let concatgxArray = (rep @  gzArray) in
-                                 aux2 tail tail2 extendL3x concatgxArray
-                               )
-                               else (
-                                 let extendL3x = extend l3x (head, head2) in
-                                 aux2 tail tail2 extendL3x rep
-                               )
-                           | _ -> (l3x,rep) in
-                      let (l3x', repl2) = aux2 l2 l3 list2 repl in
-                      fold_ex remainder l3x' repl2
-                | _ -> type_fail "Error - Please give a tuple" ) ) in
-      let (list2', repl2) = fold_ex li ctx [] in
-      let replaceRepl2 = replacing repl2 e1 in
-      infer list2' replaceRepl2
+  | Rec (_, el2, _) -> el2
   | Apply (el1, el2) -> (
       let matchInfer = infer ctx el1 in
       match matchInfer with
@@ -611,7 +493,7 @@ let rec infer (ctx : context) (e : exp) : typ =  match e with
       | _ -> type_fail "Error - No function provided, needed to apply arguments")
   | Var (var) -> (
       let matchCTX = ctx_lookup ctx var in
-      try match matchCTX with
+      match matchCTX with
         | TVar (l) -> (
             let nonL = !l in
             match nonL with
@@ -619,11 +501,65 @@ let rec infer (ctx : context) (e : exp) : typ =  match e with
             | None -> ctx_lookup ctx var
           )
         | _ -> ctx_lookup ctx var
-      with NotFound -> type_fail "Error - Free variable" )
+      )
   | Anno (var1, type1) ->
       let inferVar1 = infer ctx var1 in
       unify inferVar1 type1;
       type1
+  | Let (l,el1) ->
+      let rec aux m k =
+        let Ctx(m) = m in
+        match m with
+        | [] -> false
+        | (t, _)::remainder ->
+              aux (Ctx (remainder)) k
+      in
+      let rec fold_ex list1 list2 repl =
+        match list1 with
+        | [] -> (list2, repl)
+        | x :: remainder -> (
+            match x with
+            | Val (el1, el2) | ByName (el1, el2) -> (
+                if aux list2 el2 then (
+                  let naz = fresh_var el2 in
+                  let replaceEl1 = replfunct repl el1 in
+                  let inferEl1 = infer list2 replaceEl1 in
+                  let extendInfer =  naz, inferEl1 in
+                  let extendList2 = extend list2 (extendInfer) in
+                  let elArray = [(Var (naz)), el2] in
+                  let concatElRepl = repl @ elArray in
+                  fold_ex remainder extendList2 concatElRepl
+                )
+                else (
+                  let replace = replfunct repl el1 in
+                  let inferList2 = infer list2 replace in
+                  let extendInfer = el2, inferList2 in
+                  let extendList2 = extend list2 (extendInfer) in
+                  fold_ex remainder extendList2 repl
+                )
+              )
+            | Valtuple (l1, l2) -> (
+                let replace = replfunct repl l1 in
+                let matching = infer list2 replace in
+                match matching with
+                | TProduct (l3) ->
+                    let rec aux2 l1x l2x l3x rep =
+                      let tupLists = (l1x, l2x) in
+                           match tupLists with
+                           | ([], _) -> (l3x, rep)
+                           | (head::tail), (head2::tail2) ->
+                                 let gz = fresh_var head in
+                                 let extendL3x = extend l3x (gz,head2) in
+                                 let gzArray = [((Var (gz)), head)] in
+                                 let concatgxArray = (rep @  gzArray) in
+                                 aux2 tail tail2 extendL3x concatgxArray
+                           | _ -> (l3x,rep) in
+                      let (l3x', repl2) = aux2 l2 l3 list2 repl in
+                      fold_ex remainder l3x' repl2
+                | _ -> type_fail "Error - Please give a tuple" ) ) in
+      let (list2', repl2) = fold_ex l ctx [] in
+      let replaceRepl2 = replfunct repl2 el1 in
+      infer list2' replaceRepl2
 
 
 
